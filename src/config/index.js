ced42c43a0e20ed6c71ddec6752b02d51377dc32
liuningning <1754102225@qq.com>
2020-07-02 09:24:33 +0800

    初始化element

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  basePath: { // 用于其他属性配置 process.env.NODE_ENV 判断环境
+    port_por: 9526,
+    port_dev: 9527,
+    url_dev: 'http://localhsot:8000',
+    url_pro: 'http://localhsot:9000',
+    path: '/WMS/'
+  }
+}
